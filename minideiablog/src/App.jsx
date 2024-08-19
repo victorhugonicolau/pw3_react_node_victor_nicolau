9cbc12713ab42e20f01d014cb73d3f31a12301f8
victorhugonicolau <victor.castro29@etec.sp.gov.br>
2024-08-19 20:40:34 -0300

    Aula-0819-INstalação da biblioteca MUI React

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,12 +1,23 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ResponsiveAppBar from './navbar/navbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div>
-      <h1>Minha página em react</h1>
-    </div>
+      <div>
+        <ResponsiveAppBar />
+        <Box sx={{ width: '100%', maxWidth: 500 }}>
+          <Typography variant="h4" gutterBottom>
+            minha página da web informática da etec
+          </Typography>
+          <Button variant="contained">churinpis galilelus</Button>
+        </Box>
+      </div>
     </>
   )
 }
