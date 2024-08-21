4bd2d28d46060f19e02ce9d074027b261c5fd5bc
victorhugonicolau <victor.castro29@etec.sp.gov.br>
2024-08-21 20:41:04 -0300

    Aula - Aprendendo a usar o MUI e suas funcionalidades, estrutura base de componentes navbar

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,22 +1,12 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import ResponsiveAppBar from './navbar/navbar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import './App.css'
-
+import NavBar
+ from './componentes/navbar'
 function App() {
 
   return (
     <>
       <div>
-        <ResponsiveAppBar />
-        <Box sx={{ width: '100%', maxWidth: 500 }}>
-          <Typography variant="h4" gutterBottom>
-            minha página da web informática da etec
-          </Typography>
-          <Button variant="contained">churinpis galilelus</Button>
-        </Box>
+          <NavBar/>
       </div>
     </>
   )
